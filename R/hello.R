0d8c47b106e7be55ad8080097715671d69c45978
MarkSTenenbaum <mt2082a@american.edu>
2022-10-03 13:55:44 -0400

    First draft of package

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,69 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+library(tidyverse)
+options(dplyr.summarise.inform = FALSE)
 
-hello <- function() {
-  print("Hello, world!")
+
+addSmallLegend <- function(myPlot, pointSize = 0.5, textSize = 8, spaceLegend = 1) {
+  myPlot +
+    guides(shape = guide_legend(override.aes = list(size = pointSize)),
+           color = guide_legend(override.aes = list(size = pointSize))) +
+    theme(legend.title = element_text(size = textSize),
+          legend.text  = element_text(size = textSize),
+          legend.key.size = unit(spaceLegend, "lines"))
+}
+
+library(showtext)
+library(ggthemes)
+library(ggsci)
+
+# Plot theme
+theme_gg <- function(base_size = 14, base_family = "font", legend = FALSE){
+  font_add_google('Alegreya Sans', "font")
+  showtext_auto()
+  showtext_opts(dpi = 300)
+
+  if(legend == TRUE){
+    theme_minimal() +
+      theme(legend.position = "bottom") +
+      theme(legend.title = element_blank()) +
+      theme(text=element_text(size=14, family="font"))
+  }
+  else{
+    theme_minimal() +
+      theme(legend.position = "none") +
+      theme(legend.title = element_blank()) +
+      theme(text=element_text(size=14, family="font"))
+  }
+}
+# weighted proportions
+propfuncwt <- function(data, var, weight, group){
+  if(missing(group)){
+    probs2 <- data %>%
+      dplyr::count({{var}}, wt = {{weight}}) %>%
+      mutate(pct = prop.table(n))
+    return(probs2)
+  }
+  else{
+    probs <- data %>%
+      group_by({{ group }}) %>%
+      dplyr::count({{var}}, wt = {{weight}}) %>%
+      mutate(pct = prop.table(n))
+    return(probs)
+  }
+}
+
+# unweighted proportions
+propfunc <- function(data, var, group){
+  if(missing(group)){
+    probs2 <- data %>%
+      dplyr::count({{var}}) %>%
+      mutate(pct = prop.table(n))
+    return(probs2)
+  }
+  else{
+    probs <- data %>%
+      group_by({{ group }}) %>%
+      dplyr::count({{var}}) %>%
+      mutate(pct = prop.table(n))
+    return(probs)
+  }
 }
